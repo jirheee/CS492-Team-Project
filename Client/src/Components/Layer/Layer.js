e7a6e87e6dc31450cd71ae6ce6356587c8b8e5a6
JungInRhee <ysrheee@kaist.ac.kr>
2021-10-09 15:20:54 +0900

    feat: set minimum node as 2

--- Client/src/Components/Layer/Layer.js
+++ Client/src/Components/Layer/Layer.js
@@ -1,30 +1,34 @@
 import { IconButton } from "@material-ui/core";
 import { AddCircle, RemoveCircle } from "@material-ui/icons";
 import { useState } from "react";
-import Node from "../Node/Node"
+import Node from "../Node/Node";
 
 const Layer = () => {
-    const [numNodes, setNumNodes] = useState(2);
+  const [numNodes, setNumNodes] = useState(2);
 
-    const Nodes = Array.from(Array(numNodes)).map((i) => <Node key={i}/>);
+  const Nodes = Array.from(Array(numNodes)).map((_, i) => <Node key={i} />);
 
-    const IncreaseNodes = () => {
-        setNumNodes(numNodes + 1)
+  const IncreaseNodes = () => {
+    setNumNodes(numNodes + 1);
+  };
+
+  const DecreaseNodes = () => {
+    if (numNodes > 2) {
+      setNumNodes(numNodes - 1);
     }
+  };
 
-    const DecreaseNodes = () => {
-        setNumNodes(numNodes - 1)
-    }
-
-    return <div className="layer">
-        <IconButton aria-label="Add" onClick={IncreaseNodes} color="primary">
-            <AddCircle/>
-        </IconButton>
-        <IconButton aria-label="Delete" onClick={DecreaseNodes} color="secondary">
-            <RemoveCircle/>
-        </IconButton>
-        {Nodes}
+  return (
+    <div className="layer">
+      <IconButton aria-label="Add" onClick={IncreaseNodes} color="primary">
+        <AddCircle />
+      </IconButton>
+      <IconButton aria-label="Delete" onClick={DecreaseNodes} color="secondary">
+        <RemoveCircle />
+      </IconButton>
+      {Nodes}
     </div>
-}
+  );
+};
 
 export default Layer;
