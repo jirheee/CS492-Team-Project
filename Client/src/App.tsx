b6f7bf2cf5e59623592a52c63ea36765ef766a3a
JungInRhee <ysrheee@kaist.ac.kr>
2021-11-01 17:19:07 +0900

    Fix import statement error due to case sensitivity

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { MainIndexPage, AgentIndexPage, BattleIndexPage } from './pages';
