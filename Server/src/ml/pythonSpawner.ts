9f158c06c9266f3f699f718782ef2aabe4b87acb
JungInRhee <ysrheee@kaist.ac.kr>
2021-12-17 19:28:24 +0900

    Update Python Spawner to store history

--- Server/src/ml/pythonSpawner.ts
+++ Server/src/ml/pythonSpawner.ts
@@ -1,6 +1,19 @@
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
 import { ProcessType } from '../types';
 
+interface TrainHistory {
+  epoch: number;
+  elapsed_time: number;
+  loss: number;
+  entropy: number;
+  kl: number;
+}
+
+interface WinHistory {
+  epoch: number;
+  win_ratio: number;
+}
+
 class PythonSpawner {
   private process: ChildProcessWithoutNullStreams;
 
@@ -10,15 +23,19 @@ class PythonSpawner {
 
   private options: string[];
 
-  private events: { onData: (data: string) => void };
+  public events: { onData: (data: string) => void; onExit?: () => void };
 
   private processType: ProcessType;
+
+  private trainingHistory: TrainHistory[] = [];
+
+  private winRateHistory: WinHistory[] = [];
 
   constructor(
     path: string,
     file: string,
     options: string[] = [],
-    events: { onData: (data: string) => void },
+    events: { onData: (data: string) => void; onExit?: () => void },
     processType: ProcessType = ProcessType.Train
   ) {
     this.path = path;
@@ -40,12 +57,38 @@ class PythonSpawner {
         console.log('disconected ');
       });
 
+      this.process.on('exit', () => {
+        console.log('exit');
+        if (this.events.onExit !== undefined) {
+          this.events.onExit();
+        }
+      });
+
       this.process.stdout.on('data', data => {
-        this.events.onData(String(data));
+        try {
+          const parsed: TrainHistory | WinHistory = JSON.parse(data);
+
+          if (
+            this.processType === ProcessType.Train &&
+            Object.keys(parsed).find(key => key === 'loss')
+          ) {
+            this.trainingHistory.push(parsed as TrainHistory);
+          }
+          if (
+            this.processType === ProcessType.Train &&
+            Object.keys(parsed).find(key => key === 'win_ratio')
+          ) {
+            this.winRateHistory.push(parsed as WinHistory);
+          }
+          this.events.onData(String(data));
+        } catch (e) {
+          console.error('Parse failed', String(data));
+        }
       });
 
       this.process.stdout.on('exit', () => {
         console.log('process finished');
+
         resolve('end');
       });
 
@@ -54,6 +97,14 @@ class PythonSpawner {
         reject(data);
       });
     });
+  }
+
+  public getTrainingHistory() {
+    return this.trainingHistory;
+  }
+
+  public getWinRateHistory() {
+    return this.winRateHistory;
   }
 }
 
