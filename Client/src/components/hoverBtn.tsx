42349c6544b8fba0d8b56f4c1544daebddb86caf
JungInRhee <ysrheee@kaist.ac.kr>
2021-12-19 17:24:41 +0900

    Modify hoverButton props

--- Client/src/components/hoverBtn.tsx
+++ Client/src/components/hoverBtn.tsx
@@ -1,13 +1,15 @@
+import React, { PropsWithChildren } from 'react';
 import { VStack, Heading, Text } from '@chakra-ui/layout';
 import { useState } from 'react';
 
 const HoverBtn = ({
   baseText,
-  hoverText
-}: {
+  hoverText,
+  children
+}: React.PropsWithChildren<{
   baseText: string;
   hoverText: string;
-}) => {
+}>) => {
   const [hover, setHover] = useState(false);
   const handleMouseOver = () => {
     setHover(true);
@@ -30,6 +32,7 @@ const HoverBtn = ({
     >
       <Heading textAlign="center">{baseText}</Heading>
       {hover ? <Text>{hoverText}</Text> : null}
+      {children}
     </VStack>
   );
 };
