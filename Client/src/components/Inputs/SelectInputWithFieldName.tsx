217d47538903ccba99ee15ee09630fab4cf34427
JungInRhee <ysrheee@kaist.ac.kr>
2021-12-19 17:24:28 +0900

    Modify select Input

--- Client/src/components/Inputs/SelectInputWithFieldName.tsx
+++ Client/src/components/Inputs/SelectInputWithFieldName.tsx
@@ -1,10 +1,22 @@
 import { Select } from '@chakra-ui/react';
 import InputWithFieldName from './InputWithFieldName';
 
-const SelectInputWithFieldName = ({ children, label, id }) => {
+const SelectInputWithFieldName = ({
+  children,
+  label,
+  id,
+  onChange
+}: {
+  children;
+  label;
+  id;
+  onChange?;
+}) => {
   return (
     <InputWithFieldName label={label} id={id}>
-      <Select id={id}>{children}</Select>
+      <Select id={id} onChange={onChange}>
+        {children}
+      </Select>
     </InputWithFieldName>
   );
 };
